458a0b6edfaf2f5aa42464a28f7220dfe380a719
Nicholas <nicholas.mob1010@gmail.com>
2026-06-15 14:41:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,27 +4,27 @@ const botao_selenio = document.getDocumentById("botao_selenio");
 const botao_telurio = document.getDocumentById("botao_telurio");
 const botao_polonio = document.getDocumentById("botao_polonio");
 
-botao_oxigenio.addEventListener(click, function(aparecer){
+botao_oxigenio.addEventListener("click", function(aparecer){
 
     document.getElementById("oxigenio").style.display = "block";
 });
 
-botao_enxofre.addEventListener(click, function(aparecer){
+botao_enxofre.addEventListener("click", function(aparecer){
 
     document.getElementById("enxofre").style.display = "block";
 });
 
-botao_selenio.addEventListener(click, function(aparecer){
+botao_selenio.addEventListener("click", function(aparecer){
 
     document.getElementById("selenio").style.display = "block";
 });
 
-botao_telurio.addEventListener(click, function(aparecer){
+botao_telurio.addEventListener("click", function(aparecer){
 
     document.getElementById("telurio").style.display = "block";
 });
 
-botao_polonio.addEventListener(click, function(aparecer){
+botao_polonio.addEventListener("click", function(aparecer){
 
     document.getElementById("polonio").style.display = "block";
 });
