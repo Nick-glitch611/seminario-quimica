a52f6c3e3d0cf50b66ef935927346768b3802e46
Nicholas <nicholas.mob1010@gmail.com>
2026-06-15 14:37:59 -0300

    Add event listeners for multiple buttons

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-const botao_oxigenio = document.getDocumentById("botao_oxigenio);
+const botao_oxigenio = document.getDocumentById("botao_oxigenio");
+const botao_enxofre = document.getDocumentById("botao_enxofre");
+const botao_selenio = document.getDocumentById("botao_selenio");
+const botao_telurio = document.getDocumentById("botao_telurio");
+const botao_polonio = document.getDocumentById("botao_polonio");
+
+botao_oxigenio.addEventListener(click, function(aparecer){
+
+    document.getElementById("oxigenio").style.display = "block";
+});
+
+botao_enxofre.addEventListener(click, function(aparecer){
+
+    document.getElementById("enxofre").style.display = "block";
+});
+
+botao_selenio.addEventListener(click, function(aparecer){
+
+    document.getElementById("selenio").style.display = "block";
+});
+
+botao_telurio.addEventListener(click, function(aparecer){
+
+    document.getElementById("telurio").style.display = "block";
+});
+
+botao_polonio.addEventListener(click, function(aparecer){
+
+    document.getElementById("polonio").style.display = "block";
+});
