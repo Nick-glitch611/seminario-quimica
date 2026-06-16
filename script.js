0ddfdbba9ec90baf14d008866115f5249a8b671d
Nicholas <nicholas.mob1010@gmail.com>
2026-06-16 09:59:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,79 @@ botao_oxigenio.addEventListener("click", function() {
 
     }
 });
+
+const botao_enxofre = document.getElementById("botao_enxofre");
+
+botao_enxofre.addEventListener("click", function() {
+
+    const div_enxofre = document.getElementById("enxofre");
+
+    if (div_enxofre.style.display === "none") {
+
+        div_enxofre.style.display = "block";
+
+    } else {
+
+        div_enxofre.style.display = "none";
+
+        div_enxofre.scrollIntoView({behavior: "smooth", block: "center"});
+
+    }
+});
+
+const botao_selenio = document.getElementById("botao_selenio");
+
+botao_selenio.addEventListener("click", function() {
+
+    const div_selenio = document.getElementById("selenio");
+
+    if (div_selenio.style.display === "none") {
+
+        div_selenio.style.display = "block";
+
+    } else {
+
+        div_selenio.style.display = "none";
+
+        div_selenio.scrollIntoView({behavior: "smooth", block: "center"});
+
+    }
+});
+
+const botao_telurio = document.getElementById("botao_telurio");
+
+botao_telurio.addEventListener("click", function() {
+
+    const div_telurio = document.getElementById("telurio");
+
+    if (div_telurio.style.display === "none") {
+
+        div_telurio.style.display = "block";
+
+    } else {
+
+        div_telurio.style.display = "none";
+
+        div_telurio.scrollIntoView({behavior: "smooth", block: "center"});
+
+    }
+});
+
+const botao_polonio = document.getElementById("botao_polonio");
+
+botao_polonio.addEventListener("click", function() {
+
+    const div_polonio = document.getElementById("polonio");
+
+    if (div_polonio.style.display === "none") {
+
+        div_polonio.style.display = "block";
+
+    } else {
+
+        div_polonio.style.display = "none";
+
+        div_polonio.scrollIntoView({behavior: "smooth", block: "center"});
+
+    }
+});
